49c16e36c6772d30d2fe6636bdd9d9a858746db6
mehul4mak <mehul4mak@gmail.com>
2024-07-08 00:26:19 +0530

    Update code structure and code itself

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,14 @@
 # Put the code for your API here.
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return "Hello Mehul!"
+
+
+@app.post("/predict")
+def predict():
+    return 1
